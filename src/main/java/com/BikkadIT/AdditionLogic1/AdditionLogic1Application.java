98afe35d18530750136c86231ddf931d90abffa1
Moseemp <moseemp@gmail.com>
2023-04-23 21:52:33 +0530

    this is the second commit

--- src/main/java/com/BikkadIT/AdditionLogic1/AdditionLogic1Application.java
+++ src/main/java/com/BikkadIT/AdditionLogic1/AdditionLogic1Application.java
@@ -3,11 +3,19 @@ package com.BikkadIT.AdditionLogic1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ch.qos.logback.classic.spi.PackagingDataCalculator;
+
 @SpringBootApplication
 public class AdditionLogic1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdditionLogic1Application.class, args);
+		
+		
 	}
+	public String getMsgFirst() {
+		return "ongoing work";
+	}
+
 
 }
